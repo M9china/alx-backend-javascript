27cb9392dae5a10e566cdc8870f69f3cdfce6d04
M9china <nqobilencaza2@gmail.com>
2024-12-18 12:46:25 +0200

    feat: implement Express API with cart route and corresponding unit tests

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -12,4 +12,14 @@ describe("Index Page Tests", () => {
     const response = await supertest(app).get("/");
     expect(response.text).to.include("Welcome to the payment system");
   });
+
+  it("should return 200 when id is a number", async () => {
+    let response = await supertest(app).get("/cart/12");
+    expect(response.statusCode).to.equal(200);
+  });
+
+  it("should return 404 when id is not a number", async () => {
+    const response = await supertest(app).get("/cart/twelve");
+    expect(response.statusCode).to.equal(404);
+  });
 });
