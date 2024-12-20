1e0fab3340df2f1c19d9fe3bddd8b37594047671
M9china <nqobilencaza2@gmail.com>
2024-12-20 18:05:05 +0200

    feat: streamline response handling by removing status codes for simpler output

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,16 +4,16 @@ const app = express();
 const port = 7865;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the payment system");
+  res.send("Welcome to the payment system");
 });
 
 app.get("/cart/:id(\\d+)", (req, res) => {
   const cartId = req.params.id;
-  res.status(200).send(`Payment methods for cart ${cartId}`);
+  res.send(`Payment methods for cart ${cartId}`);
 });
 
 app.use((req, res) => {
-  res.status(404).send("Not found");
+  res.send("Not found");
 });
 
 app.listen(port, () => {
