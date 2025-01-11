041f8ffaac18dfb45ac0f51766c414889da8bd19
M9china <nqobilencaza2@gmail.com>
2025-01-11 03:05:13 +0200

    feat: standardize string quotes to single quotes in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 const isInputPipped = process.stdin.isTTY;
 
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
   if (isInputPipped) {
@@ -10,8 +10,8 @@ process.stdin.on("data", (data) => {
   }
 });
 
-process.on("exit", () => {
+process.on('exit', () => {
   if (!isInputPipped) {
-    console.log("This important software is now closing");
+    console.log('This important software is now closing');
   }
 });
